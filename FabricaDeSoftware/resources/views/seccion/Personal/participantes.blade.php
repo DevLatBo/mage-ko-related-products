8efb61ed3850d92e94634bf8e94a7f8c572c9053
irvin373 <irvin373@gmail.com>
2016-09-15 19:16:52 -0400

    people

--- FabricaDeSoftware/resources/views/seccion/Personal/participantes.blade.php
+++ FabricaDeSoftware/resources/views/seccion/Personal/participantes.blade.php
@@ -1,28 +1,27 @@
 @extends('seccion.Personal.personal')
 
 @section('contenido')
-	<div class="panel-heading">
-        <h3 class="panel-title">{{$personal}}</h3>
-    </div>
-    <div class="panel-body" align="justify">
-		<table class="table table-striped" id="cssTable">
-			<thead class="cabeceraPersonal">
-				<th colspan=2>NOMBRE PERSONAL</th>
-				<th>AREAS DE INVESTIGACION</th>
-			</thead>
-			<tbody>
-				@foreach($participantes as $p)
-					<tr>
-						<td class="col-lg-3" align="center"><img src="/Fotos/{{$p->FOTO}}" width=60%></td>
-						<td class="col-lg-4">
-							{{$p->NOMBRE}}<br>
-							{{$p->CORREO}}<br>
-							{{$p->CARRERA}}
-						</td>
-						<td class="col-lg-5">{{$p->AREAS}}</td>
-					</tr>
-				@endforeach
-			</tbody>
-		</table>
-	</div>
+
+<h3>{{$personal}}</h3>
+<div class="row">
+@foreach($participantes as $p)
+    <div class="col s12 m6">
+	<div class="card horizontal">
+	    <div class="card-image activator waves-effect waves-block waves-light">
+	      <img class="activator" src="http://lorempixel.com/100/190/nature/6">
+	    </div>
+	    <div class="card-content">
+	      <span class="card-title grey-text text-darken-4">{{$p->NOMBRE}}<i class="activator horizontal material-icons right">more_vert</i></span>
+	      <p>{{$p->CORREO}}</p>	
+	      <p>{{$p->CARRERA}}</p>
+	    </div>
+	    <div class="card-reveal">
+	      <span class="card-title grey-text text-darken-4">Areas<i class="material-icons right">close</i></span>
+	      {{$p->AREAS}}	      
+	    </div>
+	  </div>
+</div>
+@endforeach
+</div>
+
 @stop
