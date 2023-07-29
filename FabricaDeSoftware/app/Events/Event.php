b41b6222c2718fd66f6fd78744c24cad7b5c3e86
05K4R1N <oskr.cbba2010@gmail.com>
2023-07-28 20:22:06 -0400

    [D] Removing old project

--- FabricaDeSoftware/app/Events/Event.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Fabrica\Events;
-
-abstract class Event
-{
-    //
-}
--- FabricaDeSoftware/resources/views/Multimedia/lista.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.admin')
-@section('content')
-	<div class="container well col-lg-12">
-		{{Html::linkAction("AdmiController@verFormularioMultimedia","Agregar Foto",array("form"=>"foto"),array("class"=>"btn btn-primary"))}}
-		{{Html::linkAction("AdmiController@verFormularioMultimedia","Agregar Video",array("form"=>"video"),array("class"=>"btn btn-primary"))}}
-	</div>
-@stop
